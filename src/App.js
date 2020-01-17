77b198ad95f0154c503230da2d459be69e38c4e6
Samuel Phineas Hecht <hechtsp@gmail.com>
2020-01-16 18:05:55 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import axios from 'axios';
+
+
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      weather: {},
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`https://cors-anywhere.herokuapp.com/https://api.forecast.io/forecast/cf20d2bc8131e875da42c2f5f85d7282/37.9164,-122.3481?exclude=[,minutely,flags,]`)
+      .then(res => {
+        const weather = res.data;
+        this.setState({ weather });
+      })
+  }
+
+
+  render() {
   return (
     <div className="App">
       <header className="App-header">
@@ -23,4 +43,5 @@ function App() {
   );
 }
 
+}
 export default App;
